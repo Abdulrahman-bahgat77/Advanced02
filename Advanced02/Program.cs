c6689aeb8bf16a62bd6774071dabd3a60960833e
mano <mano@DESKTOP-G5FO16R>
2025-01-17 23:13:09 +0200

    Add project files.

---
+++ Advanced02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Advanced02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
